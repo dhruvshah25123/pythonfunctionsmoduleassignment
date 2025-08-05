0032b085783c189e03e0dae4540d76de14709d4d
dhruvshah <dhruvshah6804@gmail.com>
2025-08-05 16:23:30 +0530

    initial commit

---
+++ task2.py
@@ -0,0 +1,8 @@
+import math
+n = int(input("Enter a number: "))
+squareroot = math.sqrt(n)
+print(f"The square root of {n} is {squareroot}.")
+logarithm = math.log(n)
+print(f"The logarithm of {n} is {logarithm}.")
+sin = math.sin(n)
+print(f"The sin of {n} is {sin}.")
